41af4d301e9fb751f36f2a3c93ae789e3f4992b5
Rares Neagu <neagu1000@gmail.com>
2017-01-08 22:12:24 +0200

    Added 404 message.

--- views/404.php
+++ views/404.php
@@ -4,6 +4,13 @@
  * User: xiodine
  * Date: 1/8/2017
  * Time: 6:19 PM
+ * @var $engine \App\ViewRenderer
  */
+
 ?>
-404
+<div class="jumbotron text-center">
+    <h1>404</h1>
+    <h2>Page Not Found</h2>
+    <br>
+    <img src="<?= $engine::route("/res/img/404.jpg") ?>" alt="404 cat">
+</div>
