3123a646eea26161c3c56014ca010d15a9b4325c
Rares Neagu <neagu1000@gmail.com>
2017-01-18 05:11:42 +0200

    Updated SQL Query.

--- create.sql
+++ create.sql
@@ -1,43 +1,5 @@
 # Create SQL Query
 
-CREATE TABLE categories
-(
-  id        INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  name      TEXT                NOT NULL,
-  parent_id INT(11),
-  CONSTRAINT categories_categories_id_fk FOREIGN KEY (parent_id) REFERENCES categories (id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
-);
-CREATE INDEX categories_categories_id_fk
-  ON categories (parent_id);
-
-
-CREATE TABLE roles
-(
-  id   INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  name TEXT                NOT NULL
-);
-
-
-CREATE TABLE userroles
-(
-  user_id INT(11) NOT NULL,
-  role_id INT(11) NOT NULL
-);
-
-
-CREATE TABLE users
-(
-  id       INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  name     TEXT                NOT NULL,
-  email    VARCHAR(64)         NOT NULL,
-  password VARCHAR(64)         NOT NULL
-);
-CREATE UNIQUE INDEX email
-  ON users (email);
-
-
 CREATE TABLE books
 (
   id          INT(11) PRIMARY KEY                                                                                                                           NOT NULL AUTO_INCREMENT,
@@ -46,7 +8,6 @@ CREATE TABLE books
   title       TEXT                                                                                                                                          NOT NULL,
   link        TEXT                                                                                                                                          NOT NULL,
   created_at  DATETIME DEFAULT CURRENT_TIMESTAMP                                                                                                            NOT NULL,
-  description VARCHAR(255),
   thumbnail   VARCHAR(255) DEFAULT 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg==' NOT NULL,
   CONSTRAINT books_categories_id_fk FOREIGN KEY (category_id) REFERENCES categories (id)
     ON DELETE CASCADE,
@@ -58,8 +19,29 @@ CREATE INDEX books_categories_id_fk
   ON books (category_id);
 CREATE INDEX books_users_id_fk
   ON books (user_id);
-
-
+CREATE TABLE caches
+(
+  id         INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  value      LONGBLOB            NOT NULL,
+  name       VARCHAR(255)        NOT NULL,
+  expires_at DATETIME            NOT NULL
+);
+CREATE TABLE categories
+(
+  id        INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name      TEXT                NOT NULL,
+  parent_id INT(11),
+  CONSTRAINT categories_categories_id_fk FOREIGN KEY (parent_id) REFERENCES categories (id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+CREATE INDEX categories_categories_id_fk
+  ON categories (parent_id);
+CREATE TABLE roles
+(
+  id   INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name TEXT                NOT NULL
+);
 CREATE TABLE settings
 (
   id    INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -68,12 +50,17 @@ CREATE TABLE settings
 );
 CREATE UNIQUE INDEX settings_name_uindex
   ON settings (name);
-
-
-CREATE TABLE caches
+CREATE TABLE userroles
 (
-  id         INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  value      TEXT                NOT NULL,
-  name       VARCHAR(255)        NOT NULL,
-  expires_at DATETIME            NOT NULL
+  user_id INT(11) NOT NULL,
+  role_id INT(11) NOT NULL
 );
+CREATE TABLE users
+(
+  id       INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name     TEXT                NOT NULL,
+  email    VARCHAR(64)         NOT NULL,
+  password VARCHAR(64)         NOT NULL
+);
+CREATE UNIQUE INDEX email
+  ON users (email);
